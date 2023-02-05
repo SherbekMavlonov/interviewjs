04a4919215eb22299a1725f3ab2f591b8237ea19
SHERBEK <mavlonovsherbek43@gmail.com>
2023-02-05 14:37:00 +0500

    WIP: routing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _navigations: './src/navigations',
+          _scenes: './src/scenes',
+          _services: './src/services',
+          _styles: './src/styles',
+          _utils: './src/utils',
+        },
+      },
+    },
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.json',
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+        ],
+        alias: {
+          '@components': './src/components',
+          '@navigation': './src/navigation',
+          '@typings': './src/typings',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@assets': './src/assets',
+          '@features': './src/features',
+          '@services': './src/services',
+        },
+      },
+    ],
+  ],
 };
